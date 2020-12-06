18b04c356e9392cabfebff0171af6c939ea08278
roberto <rmorelli@bo.infn.it>
2020-12-06 09:43:01 +0100

    first release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='covid_fit',
-      version='0.1',
+      version='0.2',
       description='Fit utilities to analyze covid data',
       author='Roberto Morelli',
       author_email='roberto.morelli7@unibo.it',
