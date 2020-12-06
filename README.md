9f7cd8aa13ce25cf9eea224c60110361dffabb3d
roberto <rmorelli@bo.infn.it>
2020-12-06 09:56:51 +0100

    third release

---
+++ README.md
@@ -0,0 +1,3 @@
+This package allow:
+1)Dowload more recent csv file fromm protezione civile repository 
+2)Make same simple fit analysis to study the trend in all regioni and province italiane
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='covid_fit',
-      version='0.2',
+      version='0.3',
       description='Fit utilities to analyze covid data',
       author='Roberto Morelli',
       author_email='roberto.morelli7@unibo.it',
