5fa06e2f956b4f894dd07668a305ec65534dd6bc
roberto <rmorelli@bo.infn.it>
2020-12-09 12:29:31 +0100

    requirementes in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import setuptools
 
 setuptools.setup(name='covid_fit',
-      version='0.3',
+      version='0.3.1',
       description='Fit utilities to analyze covid data',
       author='Roberto Morelli',
       author_email='roberto.morelli7@unibo.it',
       url='https://github.com/robomorelli/covid_fit',
       packages=setuptools.find_packages(),
+      install_requires=["requests", "pandas", "scipy", "numpy", "matplotlib", "datetime"],
      )
